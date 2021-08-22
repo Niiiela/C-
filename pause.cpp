c0ccf317ba750b4586dfcb4c75fa6cac924c19d9
Daniela Vieira <danielavieira2024@gmail.com>
2021-08-21 22:32:18 -0300

    Add files via upload

---
+++ pause.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <locale.h>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Portugueses");
+	
+	
+	system("pause");
+	return 0;
+}
